9d86e7177fb0e93f09e20f753e3470e5d6fffde5
grantj-re3 <grantj-re3@github.fake>
2018-09-18 12:02:13 +0930

    Permit trial-access to a database by allowing an expiry date to be specified

--- bin/ezp_merge_into_group_stanzas.sh
+++ bin/ezp_merge_into_group_stanzas.sh
@@ -29,7 +29,7 @@ delim="|"			# Single char delimiter for sort command
 # - upper or lower or mixed case
 # - only first 2 letters are considered for matching
 # - fairly flexible regarding spaces (except 2-letter match must occur either
-#   immediately after a space or immediately after $prefix_re).
+#   immediately after a space or immediately after $re_groups).
 #
 # Examples:
 #   # @groups: fl fm at al
@@ -37,7 +37,9 @@ delim="|"			# Single char delimiter for sort command
 #   # @groups: FLINders FMC Atl alumNI
 
 # Needs to be unique so we don't accidently match a "normal" comment.
-prefix_re="^# *@groups:"
+re_delim=":"
+re_groups="^# *@groups$re_delim"
+re_trial_last_day="^# *@triallastday$re_delim"
 
 # Directives which appear before every stanza (one per line)
 pre_directives="
@@ -58,6 +60,10 @@ config="
 CAT_RAW="cat"					# Copy stdin to stdout
 CAT_CLEAN="perl -pe 's/[^[:ascii:]]//g'"	# Ditto, but strip non-ascii chars
 CAT="$CAT_CLEAN"				# Set to CAT_RAW or CAT_CLEAN
+
+# Define true and false for bash/sh
+TRUE=1
+FALSE=""
 
 ##############################################################################
 show_pre_directives() {
@@ -94,21 +100,53 @@ get_sorted_file_list() {
 }
 
 ##############################################################################
+will_include_this_file() {
+  fpath_stanza="$1"
+  will_include=$TRUE		# Assume we will include this stanza-file
+
+  trial_last_day_line=`egrep -i "$re_trial_last_day" "$fpath_stanza" |head -1`
+  [ "$trial_last_day_line" ] && {
+    fname_stanza=`basename "$fpath_stanza"`
+
+    # This is a trial. Exclude if we are past the trial-expiry date.
+    trial_last_day=`echo "$trial_last_day_line" |sed "s!^.*$re_delim *!!;"' s! *$!!'`
+    trial_last_day_yyyymmdd=`date -d "$trial_last_day" '+%Y-%m-%d' 2>/dev/null`	# YYYY-MM-DD
+    [ $? != 0 -o -z "$trial_last_day" ] && {
+      will_include=$FALSE
+      echo "Invalid date '$trial_last_day'. Excluding trial stanza-file: $fname_stanza"
+
+    } || {
+      today=`date '+%Y-%m-%d'`	# YYYY-MM-DD
+      [ $today \> "$trial_last_day_yyyymmdd" ] && {
+        will_include=$FALSE
+        echo "Last day $trial_last_day_yyyymmdd. Expired trial stanza-file: $fname_stanza"
+
+      } || {
+        echo "Last day $trial_last_day_yyyymmdd. Including active trial stanza-file: $fname_stanza"
+      }
+    }
+  }
+}
+
+##############################################################################
 create_group_file() {
   match="$1"
   fpath_out="$2"
   echo
   echo "Merging stanza-files (matching '$match') into $fpath_out"
 
-  echo						> "$fpath_out"
+  echo							> "$fpath_out"
   get_sorted_file_list |while read fpath_stanza; do
-    if egrep -iq "$prefix_re($match|.* $match)" "$fpath_stanza"; then
-      fname_stanza=`basename "$fpath_stanza"`
+    if egrep -iq "$re_groups($match|.* $match)" "$fpath_stanza"; then
+      will_include_this_file "$fpath_stanza"
+      if [ "$will_include" ]; then
+        fname_stanza=`basename "$fpath_stanza"`
 
-      show_pre_directives			>> "$fpath_out"
-      echo "# From stanza-file: $fname_stanza"	>> "$fpath_out"
-      eval "$CAT \"$fpath_stanza\""		>> "$fpath_out"
-      echo					>> "$fpath_out"
+        show_pre_directives				>> "$fpath_out"
+        echo "# From stanza-file: $fname_stanza"	>> "$fpath_out"
+        eval "$CAT \"$fpath_stanza\""			>> "$fpath_out"
+        echo						>> "$fpath_out"
+      fi
     fi
   done
 }
